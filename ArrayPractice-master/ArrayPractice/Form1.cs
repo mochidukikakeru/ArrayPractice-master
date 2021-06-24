25300bbb82935915f71973bb4e2b84d581a4c3ba
ookamiyoujyo <ookamiyoujyo@gmail.com>
2021-06-24 09:46:22 +0900

    totyuu

--- ArrayPractice-master/ArrayPractice/Form1.cs
+++ ArrayPractice-master/ArrayPractice/Form1.cs
@@ -24,6 +24,10 @@ namespace ArrayPractice
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Height - label2.Height);
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Height - label3.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -33,6 +37,8 @@ namespace ArrayPractice
 
             label1.Left += vx;
             label1.Top += vy;
+            label2.Left += vx;
+            label2.Top += vy;
 
             if (label1.Left < 0)
             {
@@ -51,15 +57,47 @@ namespace ArrayPractice
                 vy = -Math.Abs(vy);
             }
 
+            if (label2.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label2.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label2.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label2.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+
             Point fpos = PointToClient(MousePosition);
 
             if ((fpos.X >= label1.Left)
                 && (fpos.X < label1.Right)
                 && (fpos.Y >= label1.Top)
                 && (fpos.Y < label1.Bottom))
-            {
+
+                if ((fpos.X >= label2.Left)
+                && (fpos.X < label2.Right)
+                && (fpos.Y >= label2.Top)
+                && (fpos.Y < label2.Bottom))
+                {
                 timer1.Enabled = false;
             }
+        }
+
+        private void scoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
